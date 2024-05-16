c21482e3c6a83325426815edc64671f74b5092f2
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-05-16 22:25:40 +0500

    home api view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from products.models import Product
 
-# Create your views here.
+
+def api_home(request):
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+    return JsonResponse(data)
